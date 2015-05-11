02f1d9f65bfe5c7a41741d930ef3d81c8ef4e6f4
ArunJayan <arunjayan32@gmail.com>
2015-05-11 18:29:13 +0530

    modified opencv-drawing-fun.py with little more examples with lib. funs

--- opencv-drawing-fun.py
+++ opencv-drawing-fun.py
@@ -14,3 +14,37 @@ cv2.waitKey(0)
 cv2.line(img,(200,200),(560,30),(0,255,0),4)
 cv2.imshow("drawing functions2",img)
 cv2.waitKey(0)
+# now we can draw a rectangle using cv2.rectangle()
+cv2.rectangle(img,(30,30),(400,400),(255,0,0),2)
+cv2.imshow("drawing functions3",img)
+cv2.waitKey(0)
+"""
+first argument of cv2.rectangle() : numpy array of image
+second argument of cv2.rectangle() : first coordinate of rectangle (left top)
+third argument of cv2.rectangle() : final coordinate of rectangle (right bottom)
+fourth argument of cv2.rectangle() : colour
+fifth : thickness 
+"""
+#another example
+cv2.rectangle(img,(100,100),(400,550),(0,240),1)
+cv2.imshow("drawing function4",img)
+cv2.waitKey(0)
+## now we can draw a circle using cv2.circle()
+cv2.circle(img,(300,300),50,(0,250,200))
+## fist parameter of fun is numpy array
+## second is coordinates of center
+## third is radius
+## fourth is colour
+cv2.imshow("drawing function5",img)
+cv2.waitKey(0)
+#another example
+img2 = np.zeros((500,500),dtype="uint8")
+for a in range(1,200,25):
+	cv2.circle(img2,(250,250),a,(255,0,0))
+for a in range(70):
+	cv2.circle(img,(250,250),a,(255,0,0))	
+cv2.imshow("pattern",img2)
+cv2.waitKey(0)
+cv2.imshow("modified",img)
+cv2.waitKey(0)
+

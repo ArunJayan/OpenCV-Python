62c01e75101a60956cf9ae30b4ea94c29d195b2e
ArunJayan <arunjayan32@gmail.com>
2015-05-30 01:26:38 +0530

    added transilate() to im_functiona lib

--- im_functions.py
+++ im_functions.py
@@ -1,3 +1,7 @@
 import numpy as np
 import cv2
-
+#function that can be used transilation of images
+def translate(img,tx,ty):
+	M = np.float32([[1,0,tx],[0,1,ty]])
+	shifted = cv2.warpAffine(img,M,(img.shape[1],img.shape[0]))
+	return shifted

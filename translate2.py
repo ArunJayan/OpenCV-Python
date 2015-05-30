9d0016ef27a151df3ae2e8377d4e7d13dba1b86a
ArunJayan <arunjayan32@gmail.com>
2015-05-30 09:13:46 +0530

    change the name

--- translate2.py
+++ translate2.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
-from im_functions import transilate
+from im_functions import translate
 
 img = cv2.imread("images/wave.png")
-cv2.imshow(0)
+cv2.imshow("img",img)
 cv2.waitKey(0)
-trns = transilate(img,-90,-50)
+trns = translate(img,-90,-50)
 cv2.imshow("transilated img",trns)
 cv2.waitKey(0)

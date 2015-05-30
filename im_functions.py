ea887b2deee7d497b4ac6cd135979eb643a22cbb
ArunJayan <arunjayan32@gmail.com>
2015-05-30 10:50:38 +0530

    im_functions.py modified

--- im_functions.py
+++ im_functions.py
@@ -5,13 +5,12 @@ def translate(img,tx,ty):
 	M = np.float32([[1,0,tx],[0,1,ty]])
 	shifted_img = cv2.warpAffine(img,M,(img.shape[1],img.shape[0]))
 	return shifted_img
-def rotate(img,point=None,angle,scale=1.0):
+def rotate(img,angle,point=None,scale=1.0):
 	width = img.shape[1]
 	height = img.shape[0]
 
-	if pont is None:
+	if point is None:
 		point = (width/2,height/2)
 	M = cv2.getRotationMatrix2D(point,angle,scale)
-	rotated_img = cv2.warpAffine(img,M,(img.shape[1],img.shape[0]))
+	rotated_img = cv2.warpAffine(img,M,(width,height))
 	return  rotated_img
-	

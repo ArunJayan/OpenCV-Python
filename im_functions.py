eccd8300ac0de3ebbc456ca4da45b13572729a9e
ArunJayan <arunjayan32@gmail.com>
2015-05-30 09:13:14 +0530

    added translate()

--- im_functions.py
+++ im_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-#function that can be used transilation of images
+#function that can be used translation of images
 def translate(img,tx,ty):
 	M = np.float32([[1,0,tx],[0,1,ty]])
 	shifted = cv2.warpAffine(img,M,(img.shape[1],img.shape[0]))

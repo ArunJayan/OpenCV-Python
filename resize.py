2ca584c3a79d346283a89586d6d4703018fef869
ArunJayan <arunjayan32@gmail.com>
2015-05-31 08:07:34 +0530

    Resizing th images

--- resize.py
+++ resize.py
@@ -5,3 +5,8 @@ image = cv2.imread("images/wave.png")
 cv2.imshow("Wave.png",image)
 cv2.waitKey(0)
 #in above steps we read a image called wave.png and showed it .
+
+width = image.shape[1]	#current image's width
+height = image.shape[0]	#current image's height 
+
+print "width : %d\nheight : %d"%(width,height)

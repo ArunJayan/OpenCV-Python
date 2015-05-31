b84422a2411dfa83beeb92e72ebdfc7873e2152f
ArunJayan <arunjayan32@gmail.com>
2015-05-31 08:50:59 +0530

    added resize()

--- im_functions.py
+++ im_functions.py
@@ -14,3 +14,16 @@ def rotate(img,angle,point=None,scale=1.0):
 	M = cv2.getRotationMatrix2D(point,angle,scale)
 	rotated_img = cv2.warpAffine(img,M,(width,height))
 	return  rotated_img
+def resize(img,width=None,height=None,inter = cv2.INTER_AREA):
+	dim = None
+	(h,w) = (img.shape[0],img.shape[1])
+	if width is None and height is None:
+		return img
+	if width is None:
+		r = height/float(h)
+		dim = (int(r*w),height)
+	else:
+		r = width/float(w)
+		dim = (width,int(h*r))
+	resized = cv2.resize(img,dim,interpolation=inter)
+	return resized

21421c5d1c76d100dac4c51acee443c58d367063
ArunJayan <arunjayan32@gmail.com>
2015-05-31 08:04:38 +0530

    fix spelling mistakes

--- resize.py
+++ resize.py
@@ -1,2 +1,7 @@
 import cv2
 import numpy as np
+
+image = cv2.imread("images/wave.png")
+cv2.imshow("Wave.png",image)
+cv2.waitKey(0)
+#in above steps we read a image called wave.png and showed it .

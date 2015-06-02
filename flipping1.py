a739b631c0c5a162a1d38ff83286a7adadf4a32a
ArunJayan <arunjayan32@gmail.com>
2015-06-02 18:36:51 +0530

    modified

--- flipping1.py
+++ flipping1.py
@@ -1,3 +1,4 @@
+#in this figure we flipp the image
 import cv2
 import numpy as np
 
@@ -5,15 +6,15 @@ img = cv2.imread("images/wave.png")
 cv2.imshow("wave.png",img)
 cv2.waitKey(0)
 
-flipped1 = cv2.flip(img,1)
+flipped1 = cv2.flip(img,1)	#second argument is flipp magnitude , if place 1 ,image will horizontally
 cv2.imshow("flipped horizontal",flipped1)
 cv2.waitKey(0)
 
-flipped2 = cv2.flip(img,0)
+flipped2 = cv2.flip(img,0)#ZERO will filpp vertical
 cv2.imshow("flipped vertical",flipped2)
 cv2.waitKey(0)
 
-flipped_horiz_verti = cv2.flip(img,-1)
+flipped_horiz_verti = cv2.flip(img,-1)#-1 will flip vertical and horizontal
 cv2.imshow("flipped vertical and horizontal",flipped_horiz_verti)
 cv2.waitKey(0)
 

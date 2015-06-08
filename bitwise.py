0453be2dca662af123210f622c0717c9b6a1b77b
ArunJayan <arunjayan32@gmail.com>
2015-06-08 17:25:26 +0530

    added and operation between two images

--- bitwise.py
+++ bitwise.py
@@ -5,3 +5,12 @@ rectangle = np.zeros((300,300),dtype="uint8")
 cv2.rectangle(rectangle,(100,100),(250,250),255,-1)
 cv2.imshow("rectangle",rectangle)
 cv2.waitKey(0)
+
+circle = np.zeros((300,300),dtype="uint8")
+cv2.circle(circle,(150,150),90,255,-1)
+cv2.imshow("circle",circle)
+cv2.waitKey(0)
+
+rect_and_circle = cv2.bitwise_and(rectangle,circle)
+cv2.imshow("AND operation",rect_and_circle)
+cv2.waitKey(0)

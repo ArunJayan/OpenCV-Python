afe29012114e0e1bc851bf05cdefe42d70fc09eb
ArunJayan <arunjayan32@gmail.com>
2015-06-11 14:40:59 +0530

    adaptive_thresholding

--- adaptive_thresholding.py
+++ adaptive_thresholding.py
@@ -8,3 +8,15 @@ cv2.waitKey(0)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow("gray-scale",gray)
 cv2.waitKey(0)
+ 
+blur_img = cv2.GaussianBlur(gray,(5,5),0)
+cv2.imshow("GaussianBlur-img",blur_img)
+cv2.waitKey(0)
+
+threshold_img = cv2.adaptiveThreshold(blur_img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,5,4)
+cv2.imshow("adaptive1",threshold_img)
+cv2.waitKey(0)
+
+threshold_img2 = cv2.adaptiveThreshold(blur_img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,15,3)
+cv2.imshow("adaptive2",threshold_img2)
+cv2.waitKey(0)

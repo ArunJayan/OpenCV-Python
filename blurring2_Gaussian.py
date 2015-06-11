7b6eb09d857402fe7d54e5b6625150e51322bfba
ArunJayan <arunjayan32@gmail.com>
2015-06-11 09:47:04 +0530

    GaaussianBlur

--- blurring2_Gaussian.py
+++ blurring2_Gaussian.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 
 img = cv2.imread("images/trex.png")
-cv2.imshow("trex.png",img)
-cv2.waitKey(0)
+#cv2.imshow("trex.png",img)
+#cv2.waitKey(0)
 
-blurred = np.hstack([cv2.GaussianBlur(img,(3,3),0),
+blurred = np.hstack([img,cv2.GaussianBlur(img,(3,3),0),
 					 cv2.GaussianBlur(img,(5,5),0),
 					 cv2.GaussianBlur(img,(7,7),0)])
 cv2.imshow("GaussianBlur",blurred)

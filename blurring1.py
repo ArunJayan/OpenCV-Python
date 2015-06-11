151512ec009e1463983bcd165c8ddcea2be269b7
ArunJayan <arunjayan32@gmail.com>
2015-06-11 09:46:33 +0530

    avarage blur

--- blurring1.py
+++ blurring1.py
@@ -3,10 +3,10 @@ import cv2
 import numpy as np
 
 image = cv2.imread("images/trex.png")
-cv2.imshow("trex.png",image)
-cv2.waitKey(0)
-blurred = np.hstack([cv2.blur(image,(3,3)),
+#cv2.imshow("trex.png",image)
+#cv2.waitKey(0)
+blurred = np.hstack([image,cv2.blur(image,(3,3)),
 					cv2.blur(image,(5,5)),
 					cv2.blur(image,(7,7))])
-cv2.imshow("blurred",blurred)
+cv2.imshow("Average blur",blurred)
 cv2.waitKey(0)
